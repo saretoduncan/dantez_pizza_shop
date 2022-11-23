dcf19201723a7c558a1d2fcff038a92cb681852d
Duncan <duncan.moiyo@gmail.com>
2022-11-23 11:54:34 +0300

    initialize project

---
+++ src/components/Navbar/NavbarComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './navbar.scss'
+export const NavbarComponent: React.FC<{}> = () => {
+  return (
+    <>
+      <nav className="container-fluid _navabar">
+
+        
+      </nav>
+    </>
+  );
+};
