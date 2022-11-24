855896c779746e527b6b6c5c75ec0167a0c8a0bf
Duncan <duncan.moiyo@gmail.com>
2022-11-24 21:40:31 +0300

    add scroll button

--- src/components/ButtonComponent/Order_Btn.tsx
+++ src/components/ButtonComponent/Order_Btn.tsx
@@ -3,8 +3,9 @@ import { IButtonProp } from '../Interfaces/interface'
 import "./orderbtn.scss"
 export const Order_Btn = (props:IButtonProp) => {
     return (
-        <div>
+        <div >
             <button className='_orderbtn'>{props.buttonContent}</button>
+            
         </div>
     )
 }
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./landingPage.scss";
 import { Order_Btn } from "../ButtonComponent/Order_Btn";
+import { ScrollButton } from "../ButtonComponent/ScrollButton";
 export const LandingPage: React.FC<{}> = () => {
   return (
-    <div className=' row _landing_body'>
+    <div className=' row position-relative _landing_body'>
       <div className='col-md-6 col-12 _intro d-flex flex-column justify-content-center align-items-center'>
         <strong> We Deliver</strong>
         <p>THE BEST PIZZA TO SHARE WITH YOU FRIENDS</p>
@@ -17,6 +18,9 @@ export const LandingPage: React.FC<{}> = () => {
           src='https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvay0yMTgtZXllLTA2NjA0MDIuanBn.jpg?s=H9Vy7lFqxhQykDtyPke99xrjUMvojK1C8D2hGbgADq4'
           alt=''
         />
+      </div>
+      <div className="position-absolute _scroll_btn_div">
+       <ScrollButton />
       </div>
     </div>
   );
