4e3418617206427fa89b0c8091665f6e290d113b
Duncan <duncan.moiyo@gmail.com>
2022-11-25 22:10:58 +0300

    remove bootstrap nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import { LandingPage } from "./components/LandingPage/LandingPage";
-import { NavbarComponent } from "./components/Navbar/NavbarComponent";
+
 function App() {
   return (
     <div className='container-fluid'>
-      <NavbarComponent />
-      <LandingPage />
+     
     </div>
   );
 }
