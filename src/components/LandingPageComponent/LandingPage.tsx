35430fedf38f7f39521d21bed2665c6f3dcba35e
Duncan <duncan.moiyo@gmail.com>
2022-11-26 20:47:56 +0300

    complete text area in  landing page

--- src/components/LandingPageComponent/LandingPage.tsx
+++ src/components/LandingPageComponent/LandingPage.tsx
@@ -4,7 +4,7 @@ import { OrderBtn } from '../ButtonComponents/OrderBtn';
 
 export const LandingPage: React.FC<{}> = () => {
   return (
-    <div className='w-[100%] h-[90vh] bg-main_background grid md:grid-cols-2  grid-col-1'>
+    <div className='w-[100%] h-[90vh] bg-main_background grid md:grid-cols-2  grid-col-1 border-b-2 border-orange'>
       <section className='self-center'>
         <p className=' text-center text-orange italic font-semibold text-[25px]'>
           We deliver
