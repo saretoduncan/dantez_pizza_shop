d710fdc39f1565ef759f8a0a883c30b652734902
Duncan <duncan.moiyo@gmail.com>
2022-11-26 20:13:05 +0300

    add order now button

--- src/components/ButtonComponents/OrderBtn.tsx
+++ src/components/ButtonComponents/OrderBtn.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { IButtonProp } from '../Interfaces/interface';
 
-export const OrderBtn: React.FC<{}> = () => {
+export const OrderBtn = (prop:IButtonProp) => {
     return (
-        <div>
-            
+        <div className='flex justify-center'>
+           <button className='text-white bg-orange px-10 py-3 text-[20px] mt-5'>{prop.buttonContent}</button> 
         </div>
     )
 }
--- src/components/LandingPageComponent/LandingPage.tsx
+++ src/components/LandingPageComponent/LandingPage.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { OrderBtn } from '../ButtonComponents/OrderBtn';
+
 
 export const LandingPage: React.FC<{}> = () => {
   return (
     <div className='w-[100%] h-[90vh] bg-main_background grid md:grid-cols-2  grid-col-1'>
       <section className='self-center'>
-        <p className=' text-center text-orange italic font-semibold text-[25px]'>We deliver</p>
+        <p className=' text-center text-orange italic font-semibold text-[25px]'>
+          We deliver
+        </p>
         <p className='text-[45px] text-center font-bold text-orange'>
           The <br /> best <br /> pizza To <br /> share <br /> with <br />{" "}
           friends
         </p>
+        <OrderBtn buttonContent={"ORDER NOW"} />
       </section>
-      <section className='w-[100%]'></section>
+      <section className='w-[100%] lg:hidden'></section>
     </div>
   );
 };
